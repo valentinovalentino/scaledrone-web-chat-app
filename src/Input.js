155c6bdd2579b6db04befdba8d068b4df2f0e361
valentinovalentino <valentino.bubalo@gmail.com>
2023-06-30 14:07:05 +0200

    master - cleaned comments

--- src/Input.js
+++ src/Input.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Component } from "react";
 
-// klasna komponenta za prikaz teksta upisane poruke
 class Input extends Component {
 	state = {
 		text: "",
 	};
 
-	//funkcija klasi postavlja tekst akcije, a okida svaki put kad se promijeni unos
 	onChange(e) {
 		this.setState({ text: e.target.value });
 	}
 
-	//funkcija osigurava akciju jednom,  postavlja prvotno stanje, a upravlja isporukom poruke koju prima od app u razgovor
 	onSubmit(e) {
 		e.preventDefault();
 		this.props.onSendMessage(this.state.text);
@@ -21,7 +18,6 @@ class Input extends Component {
 
 	render() {
 		return (
-			// forma s poljem za unos teksta i gumbom za poslat tekst u razgovor
 			<div className='input'>
 				<form onSubmit={(e) => this.onSubmit(e)}>
 					<input
--- src/Messages.js
+++ src/Messages.js
@@ -1,9 +1,7 @@
 import { Component } from "react";
 import React from "react";
-// klasna komponenta za poruku
 
 class Messages extends Component {
-	// funkcija u komponenti za listu za svaku poruku s imenom, avatarom, i tekstom poruke
 	renderMessage(message) {
 		const { member, text } = message;
 		const { currentMember } = this.props;
@@ -17,22 +15,17 @@ class Messages extends Component {
 					className='avatar'
 					style={{ backgroundColor: member.clientData.color }}
 				/>{" "}
-				{/* scaledrone clientData sidro za boju korisnika */}
 				<div className='message-content'>
 					<div className='username'>{member.clientData.username}</div>{" "}
-					{/* scaledrone clientData sidro na korisnikovo ime */}
 					<div className='text'>{text}</div>
 				</div>
 			</li>
 		);
 	}
 
-	// funkcija za poruku
 	render() {
-		//varijabla mapa poruka koje proslijedimo
 		const { messages } = this.props;
 		return (
-			//lista poruka koje stvaramo u mapu
 			<ul className='messages-list'>
 				{messages.map((m) => this.renderMessage(m))}
 			</ul>
@@ -41,5 +34,3 @@ class Messages extends Component {
 }
 
 export default Messages;
-
-//ova komponenta prima prop od app komponente i koristi se za prikaz poruke u razgovoru
